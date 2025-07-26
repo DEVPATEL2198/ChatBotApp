249d246135b13c2df91deb4b5d2eec364eb93d85
Dev Patel <dev.2256.p@gmail.com>
2025-07-26 13:52:36 +0530

    Update api_constant.dart

--- lib/constant/api_constant.dart
+++ lib/constant/api_constant.dart
@@ -1,6 +1,5 @@
 class ApiConstant {
-  static const String apiKey = your apinkey here 
-";
+  static const String apiKey =" your apinkey here ";
   static const String baseUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
 }
